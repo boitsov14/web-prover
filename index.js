b2ffdcde7fb3df34e0150ef3a75cccaff453f6dc
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-05-29 20:21:38 +0900

    modify

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const txt = document.getElementById("text")
 const spinner = document.getElementById("spinner")
 const img = document.getElementById("img")
 const tex = document.getElementById("tex")
-const url = "https://web-prover.fly.dev/web/"
+const url = "https://prover-twitter-bot-docker-7go3so6c3a-an.a.run.app/web"
 
 txt.addEventListener("input", () => {
     if (txt.value === '') {
@@ -29,19 +29,19 @@ btn.onclick = async () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ 'text': txt.value })
+            body: JSON.stringify({ 'txt': txt.value })
         })
         const json = await response.json()
-        msg.innerText = json.message
-        const base64 = json.image
-        if (base64 !== "") {
+        msg.innerText = json.msg
+        const base64 = json.img
+        if (base64) {
             img.hidden = false
             img.src = 'data:image/png;base64,' + base64
             tex.hidden = false
             tex.innerText = json.tex
         }
-    } catch (error) {
-        msg.innerText = 'An unexpected error has occurred: ' + error
+    } catch (e) {
+        msg.innerText = 'An unexpected error has occurred: ' + e
     }
 
     btn.disabled = false
