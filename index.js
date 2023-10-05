313cec14fcc8f70db86989dcf1f1f96ab864b624
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-10-05 21:15:25 +0900

    check response

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const img = document.getElementById("img")
 const tex = document.getElementById("tex")
 const url = "https://prover-twitter-bot-docker-7go3so6c3a-an.a.run.app/web"
 
+// disable button if text is empty
 txt.addEventListener("input", () => {
     if (txt.value === '') {
         btn.disabled = true
@@ -15,15 +16,17 @@ txt.addEventListener("input", () => {
 })
 
 btn.onclick = async () => {
-
+    // reset
     img.hidden = true
     tex.hidden = true
     msg.innerText = ""
 
+    // disable button and show spinner
     btn.disabled = true
     spinner.hidden = false
 
     try {
+        // send request
         const response = await fetch(url, {
             method: 'POST',
             headers: {
@@ -31,6 +34,11 @@ btn.onclick = async () => {
             },
             body: JSON.stringify({ 'txt': txt.value })
         })
+        // check response
+        if (!response.ok) {
+            throw new Error(response.status + ' ' + response.statusText)
+        }
+        // show response
         const json = await response.json()
         msg.innerText = json.msg
         const base64 = json.img
@@ -44,6 +52,7 @@ btn.onclick = async () => {
         msg.innerText = 'An unexpected error has occurred: ' + e
     }
 
+    // enable button and hide spinner
     btn.disabled = false
     spinner.hidden = true
 }
